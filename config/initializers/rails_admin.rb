996939faa634b3ba6b7cde3a1e6277ab108ab02c
tienhai912 <tienhai912@yahoo.com>
2018-05-15 08:35:32 +0000

    admin gem1

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,10 +3,10 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
@@ -23,16 +23,41 @@ RailsAdmin.config do |config|
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar = true
 
+  config.main_app_name = ["Laptop World", "Admin page"]
+
+  config.model "Product" do
+    exclude_fields :order_items, :orders
+  end
+
+  config.model "Order" do
+    list do
+      exclude_fields :order_items
+    end
+  end
+
   config.actions do
     dashboard                     # mandatory
-    index                         # mandatory
-    new
+    index do
+      only ["User", "Product", "Order", "Review"]
+    end                         # mandatory
+
+    new do
+      only ["User", "Product"]
+    end
     # export
-    bulk_delete
+    bulk_delete do
+      only ["User", "Product", "Order", "Review"]
+    end
     show
-    edit
-    delete
-    show_in_app
+    edit do
+      only ["Product"]
+    end
+    delete do
+      only ["User", "Product", "Order", "Review"]
+    end
+    show_in_app do
+      only ["Product", "Review"]
+    end
 
     ## With an audit adapter, you can add:
     # history_index
